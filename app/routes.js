2e08b687c00dbe4abf344de501c3c6f7b5d541e6
Alan J. Salmoni <salmoni@gmail.com>
2021-10-27 16:27:38 +0100

    Added two routes for first page and signing in

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,11 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/v0.1/index', function(req, res) {
+	res.render('v0.1/index')
+})
+
+router.get('/v0.1/signing-in', function(req, res) {
+	res.render('v0.1/your-cases')
+})
