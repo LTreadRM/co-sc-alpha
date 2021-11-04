0ddb6599bb6abf12443ac359a7cbd76e9004fa09
Alan J. Salmoni <salmoni@gmail.com>
2021-11-04 20:24:20 +0000

    Added properly working loops
    Fingers crossed. :-/

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,21 @@ const router = express.Router()
 
 module.exports = router
 
+function initialiseVariables(req) {
+	console.log("Initialising variables now")
+	req.session.data['key_contacts'] = []
+	req.session.data['critical_events'] = []
+	req.session.save()
+	req.session.data['activeFlag'] = true
+	req.session.save()
+}
+
+function checkIfActive(req) {
+if (req.session.data['activeFlag'] == undefined || req.session.data['activeFlag'] == false) {
+	initialiseVariables(req)
+	}
+}
+
 router.get('/v0-1/index', function(req, res) {
 	res.render('v0-1/index')
 })
@@ -30,11 +45,21 @@ router.get('/v0-1/sending-about-spend', function(req, res) {
 })
 
 router.get('/v0-1/request-key-contacts', function(req, res) {
-	res.redirect('/v0-1/digital-tech-key-contacts-2a')
+	checkIfActive(req)
+	res.render('v0-1/digital-tech-key-contacts-2a')
 })
 
 router.get('/v0-1/adding-key-contact', function(req, res) {
+	/* check variables are initialised, if not, force */
+	checkIfActive(req)
 	/* take details from post and add to array of contacts */
+	var blob = []
+	blob.push(req.session.data['full-name'])
+	blob.push(req.session.data['role'])
+	blob.push(req.session.data['email'])
+	blob.push(req.session.data['today'])
+	req.session.data['key_contacts'].push(blob)
+	req.session.save()
 	res.redirect('/v0-1/digital-tech-key-contacts-2a')
 })
 
@@ -59,11 +84,29 @@ router.get('/v0-1/sending-spend-details-approvals-3', function(req, res) {
 })
 
 router.get('/v0-1/sending-spend-details-approvals-4', function(req, res) {
-	res.redirect('/v0-1/spend-details-approvals-5')
+	res.redirect('/v0-1/spend-details-approvals-5a')
 })
 
 router.get('/v0-1/sending-spend-details-approvals-5', function(req, res) {
 	res.redirect('/v0-1/digital-tech-task-list')
+})
+
+router.get('/v0-1/sending-spend-details-approvals-5a', function(req, res) {
+	/* May need to store these or perform other actions? */
+	checkIfActive(req)
+	res.redirect('/v0-1/digital-tech-task-list')
+})
+
+router.get('/v0-1/sending-spend-details-approvals-5b', function(req, res) {
+	/* Add details to array */
+	checkIfActive(req)
+	/* Get data and add to variable */
+	var blob = []
+	blob.push(req.session.data['critical-date-day'] + '/' + req.session.data['critical-date-month'] + '/' + req.session.data['critical-date-year'])
+	blob.push(req.session.data['digital-tech-describe-critical-date'])
+	req.session.data['critical_events'].push(blob)
+	req.session.save()
+	res.redirect('/v0-1/spend-details-approvals-5a')
 })
 
 router.get('/v0-1/request-digital-tech-about-project', function(req, res) {
